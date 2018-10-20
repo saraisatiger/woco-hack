bbfd7d5d17d8c4a71022ac318020c41170798638
SXH0L6Y <Sara.Hashem@fpl.com>
2018-10-20 11:38:00 -0400

    Add usage and imports

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
+"""
+	Usage:
+		--- Do this first, once ---
+		# Install python interpreter: https://www.python.org/downloads/release/python-2715/
+
+		# Install packages:
+			$ pip install flask
+			$ pip install BeautifulSoup
+			$ pip install urllib2
+			$ pip install html5lib
+
+		# Import app
+			$ export FLASK_APP = app
+		---
+
+		# Start app on http://localhost:5000
+			$ flask run
+
+		# Rerun app (after code changes)
+			$ sudo lsof -i:5000
+			$ sudo kill <the_process_running_on_port_5000>
+"""
 
 from flask import Flask
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+import html5lib
+
 app = Flask(__name__)
+html = urlopen("https://www.python.org/")
+res = BeautifulSoup(html.read(),"html5lib");
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def main():
+	return "Hello, World! It's me, new string!"
